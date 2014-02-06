a29467d1bdac045230004ff6d3e3212c8b9216eb
Sean Farrell <sean.farrell@rioki.org>
2014-02-06 20:07:46 +0100

    Adds pack method for std::vector.

--- spdr/pack.h
+++ spdr/pack.h
@@ -3,6 +3,7 @@
 #define _SPDR_PACK_H_
 
 #include <iosfwd>
+#include <vector>
 
 namespace spdr
 {    
@@ -22,6 +23,16 @@ namespace spdr
     void pack(std::ostream& os, const char* value);
     void pack(std::ostream& os, const std::string& value);
     
+    template <typename T>
+    void pack(std::ostream& os, const std::vector<T>& value)
+    {
+        pack(os, value.size());
+        for (unsigned int i = 0; i < value.size(); i++)
+        {
+            pack(os, value[i]);
+        }
+    }
+    
     void unpack(std::istream& is, bool& value);
     void unpack(std::istream& is, char& value);
     void unpack(std::istream& is, unsigned char& value);
@@ -36,6 +47,19 @@ namespace spdr
     void unpack(std::istream& is, float& value);
     void unpack(std::istream& is, double& value);
     void unpack(std::istream& is, std::string& value);
+    
+    template <typename T>
+    void unpack(std::istream& is, std::vector<T>& value)
+    {
+        size_t size;
+        unpack(is, size);
+        
+        value.resize(size);
+        for (unsigned int i = 0; i < value.size(); i++)
+        {
+            unpack(is, value[i]);
+        }
+    } 
 }
 
 #endif
