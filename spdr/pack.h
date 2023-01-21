997575ebc4e50562e6bb6aaf131923deb69d31b6
Sean Farrell <sean.farrell@rioki.org>
2023-01-21 17:49:01 +0100

    Moar noexcept.

--- spdr/pack.h
+++ spdr/pack.h
@@ -30,23 +30,23 @@
 
 namespace spdr
 {
-    SPDR_EXPORT void pack(std::ostream& os, bool value);
-    SPDR_EXPORT void pack(std::ostream& os, char value);
-    SPDR_EXPORT void pack(std::ostream& os, unsigned char value);
-    SPDR_EXPORT void pack(std::ostream& os, short value);
-    SPDR_EXPORT void pack(std::ostream& os, unsigned short value);
-    SPDR_EXPORT void pack(std::ostream& os, int value);
-    SPDR_EXPORT void pack(std::ostream& os, unsigned int value);
-    SPDR_EXPORT void pack(std::ostream& os, long value);
-    SPDR_EXPORT void pack(std::ostream& os, unsigned long value);
-    SPDR_EXPORT void pack(std::ostream& os, long long value);
-    SPDR_EXPORT void pack(std::ostream& os, unsigned long long value);
-    SPDR_EXPORT void pack(std::ostream& os, float value);
-    SPDR_EXPORT void pack(std::ostream& os, double value);
-    SPDR_EXPORT void pack(std::ostream& os, const std::string& value);
+    SPDR_EXPORT void pack(std::ostream& os, bool value) noexcept;
+    SPDR_EXPORT void pack(std::ostream& os, char value) noexcept;
+    SPDR_EXPORT void pack(std::ostream& os, unsigned char value) noexcept;
+    SPDR_EXPORT void pack(std::ostream& os, short value) noexcept;
+    SPDR_EXPORT void pack(std::ostream& os, unsigned short value) noexcept;
+    SPDR_EXPORT void pack(std::ostream& os, int value) noexcept;
+    SPDR_EXPORT void pack(std::ostream& os, unsigned int value) noexcept;
+    SPDR_EXPORT void pack(std::ostream& os, long value) noexcept;
+    SPDR_EXPORT void pack(std::ostream& os, unsigned long value) noexcept;
+    SPDR_EXPORT void pack(std::ostream& os, long long value) noexcept;
+    SPDR_EXPORT void pack(std::ostream& os, unsigned long long value) noexcept;
+    SPDR_EXPORT void pack(std::ostream& os, float value) noexcept;
+    SPDR_EXPORT void pack(std::ostream& os, double value) noexcept;
+    SPDR_EXPORT void pack(std::ostream& os, const std::string& value) noexcept;
 
     template <typename T>
-    void pack(std::ostream& os, const std::vector<T>& value)
+    void pack(std::ostream& os, const std::vector<T>& value) noexcept
     {
         pack(os, value.size());
         for (unsigned int i = 0; i < value.size(); i++)
@@ -57,33 +57,33 @@ namespace spdr
 
     template<std::size_t I = 0, typename... Tp>
     typename std::enable_if<I == sizeof...(Tp), void>::type
-    pack(std::ostream& os, const std::tuple<Tp...>& t) { }
+    pack(std::ostream& os, const std::tuple<Tp...>& t) noexcept { }
 
     template<std::size_t I = 0, typename... Tp>
     typename std::enable_if<I < sizeof...(Tp), void>::type
-    pack(std::ostream& os, const std::tuple<Tp...>& t)
+    pack(std::ostream& os, const std::tuple<Tp...>& t) noexcept
     {
         pack(os, std::get<I>(t));
         pack<I + 1, Tp...>(os, t);
     }
 
-    SPDR_EXPORT void unpack(std::istream& is, bool& value);
-    SPDR_EXPORT void unpack(std::istream& is, char& value);
-    SPDR_EXPORT void unpack(std::istream& is, unsigned char& value);
-    SPDR_EXPORT void unpack(std::istream& is, short& value);
-    SPDR_EXPORT void unpack(std::istream& is, unsigned short& value);
-    SPDR_EXPORT void unpack(std::istream& is, int& value);
-    SPDR_EXPORT void unpack(std::istream& is, unsigned int& value);
-    SPDR_EXPORT void unpack(std::istream& is, long& value);
-    SPDR_EXPORT void unpack(std::istream& is, unsigned long& value);
-    SPDR_EXPORT void unpack(std::istream& is, long long& value);
-    SPDR_EXPORT void unpack(std::istream& is, unsigned long long& value);
-    SPDR_EXPORT void unpack(std::istream& is, float& value);
-    SPDR_EXPORT void unpack(std::istream& is, double& value);
-    SPDR_EXPORT void unpack(std::istream& is, std::string& value);
+    SPDR_EXPORT void unpack(std::istream& is, bool& value) noexcept;
+    SPDR_EXPORT void unpack(std::istream& is, char& value) noexcept;
+    SPDR_EXPORT void unpack(std::istream& is, unsigned char& value) noexcept;
+    SPDR_EXPORT void unpack(std::istream& is, short& value) noexcept;
+    SPDR_EXPORT void unpack(std::istream& is, unsigned short& value) noexcept;
+    SPDR_EXPORT void unpack(std::istream& is, int& value) noexcept;
+    SPDR_EXPORT void unpack(std::istream& is, unsigned int& value) noexcept;
+    SPDR_EXPORT void unpack(std::istream& is, long& value) noexcept;
+    SPDR_EXPORT void unpack(std::istream& is, unsigned long& value) noexcept;
+    SPDR_EXPORT void unpack(std::istream& is, long long& value) noexcept;
+    SPDR_EXPORT void unpack(std::istream& is, unsigned long long& value) noexcept;
+    SPDR_EXPORT void unpack(std::istream& is, float& value) noexcept;
+    SPDR_EXPORT void unpack(std::istream& is, double& value) noexcept;
+    SPDR_EXPORT void unpack(std::istream& is, std::string& value) noexcept;
 
     template <typename T>
-    void unpack(std::istream& is, std::vector<T>& value)
+    void unpack(std::istream& is, std::vector<T>& value) noexcept
     {
         size_t size;
         unpack(is, size);
@@ -97,18 +97,18 @@ namespace spdr
 
     template<std::size_t I = 0, typename... Tp>
     typename std::enable_if<I == sizeof...(Tp), void>::type
-    unpack(std::istream& is, std::tuple<Tp...>& t) { }
+    unpack(std::istream& is, std::tuple<Tp...>& t) noexcept { }
 
     template<std::size_t I = 0, typename... Tp>
     typename std::enable_if<I < sizeof...(Tp), void>::type
-    unpack(std::istream& is, std::tuple<Tp...>& t)
+    unpack(std::istream& is, std::tuple<Tp...>& t) noexcept
     {
         unpack(is, std::get<I>(t));
         unpack<I + 1, Tp...>(is, t);
     }
 
     template <typename T>
-    T unpack(std::istream& is)
+    T unpack(std::istream& is) noexcept
     {
         auto value = T{};
         unpack(is, value);
